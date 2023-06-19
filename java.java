c818da207a9dfe8e3a66a5ec9764829e7db49aa6
Marcos <marcosmk2004@gmail.com>
2023-06-19 12:10:40 +0200

    Version Final

--- java.java
+++ java.java
@@ -1,7 +1,10 @@
-public static void multiplos(int numeroMax {
+public static void multiplos(int numeorsMax {
 
 	for (int i=0;i<=numeroMax;i++ {
 
+		if(i%7==0){
+			System.out.println("Multiplo de 7: "+i);
+		}
 		if(i%49==0){
 			System.out.println("Multiplo de 49: "+i);
 		}
